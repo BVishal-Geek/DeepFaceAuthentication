e2c076537ae420c56d081f652d40aca5939ad506
BVishal-Geek <bakshiv193@gmail.com>
2025-02-24 18:01:03 -0500

    Added capture_image function

--- mfa_face_recognition/main.py
+++ mfa_face_recognition/main.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from PIL import Image
+import numpy as np
+import cv2
 from streamlit_navigation_bar import st_navbar
 from scripts.capture_image import *
 
@@ -13,13 +16,30 @@ class app:
         st.write(self.description)
         st.write("Version: ", self.version)
     
-    def capture_image(self):
-        pass
+    def capture_image(self, disabled=None):
+        st.title("Capture Image")
+
+        # Streamlit's built-in camera capture
+        captured_image = st.camera_input("Take a picture", disabled=disabled)
+
+        if captured_image is not None:
+            # Convert Streamlit image to OpenCV format
+            image = Image.open(captured_image)
+            image = np.array(image)  # Convert PIL Image to NumPy array
+
+            # Convert RGB to BGR (OpenCV format)
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            return image  # Return the processed image
+        return None
     
     def store_new_user(self):
         st.title("Store New User")
         st.write("This is the store new user page")
-
+        enable = st.checkbox("Enable Camera")
+        picture = self.capture_image(disabled=not enable)
+        if picture:
+            st.image(picture, caption="Captured Image", use_column_width=True)
         self.name = st.text_input("Enter your name")
         self.email = st.text_input("Enter your email")
         
